ba9207931b23e7db3a241d3486cd91899f701218
Tessa Grainger <tessa.robyn@outlook.com>
2022-08-14 12:53:41 +0100

    Basic setup of cards and basic tab switching

--- page.js
+++ page.js
@@ -6,6 +6,7 @@ export class Page {
   setup() {
     this.container = document.getElementById("content");
     this.container.innerText = "";
+    this.container.className = "content";
     const head = document.createElement("h2");
     head.innerText = "Select tab type:" + String(this.id);
     this.container.appendChild(head);
@@ -20,21 +21,28 @@ export class Page {
       option.appendChild(p);
       const icon = document.createElement("img");
       icon.src = "img/" + images[i] + "Thumbnail.PNG";
+      icon.id = images[i];
       icon.classList.add("icon");
       icon.addEventListener("click", () => {
-        newTabType();
+        this.newTabType(icon);
       });
       option.appendChild(icon);
       grid.appendChild(option);
     }
     this.container.appendChild(grid);
   }
+
   load() {
-    this.setup();
+    if (!this.type) {
+      this.setup();
+    } else {
+      this.setupTabType();
+    }
   }
 
   deleted() {
     this.container.innerText = "";
+    this.container.className = "content";
     this.id;
     const head = document.createElement("h2");
     head.innerText = "Tab deleted";
@@ -42,5 +50,67 @@ export class Page {
     const p = document.createElement("p");
     p.innerText = "Choose one of your existing tabs or create a new one";
     this.container.appendChild(p);
+  }
+
+  setupAddButton() {
+    this.button = document.createElement("button");
+    this.button.innerText = "+";
+    this.button.classList.add("addButton");
+    this.container.appendChild(this.button);
+  }
+
+  newCard() {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    this.cards.push(card);
+    return card;
+  }
+
+  addCard() {
+    this.container.appendChild(this.newCard());
+  }
+
+  setupCards() {
+    this.setupAddButton();
+    this.cards = [];
+    this.button.addEventListener("click", () => {
+      this.addCard();
+    });
+    this.container.classList.add("cardsContainer");
+  }
+
+  setupImages() {
+    console.log("images coming soon!");
+  }
+
+  setupStickyNotes() {
+    console.log("sticky notes coming soon!");
+  }
+
+  setupWhiteboard() {
+    console.log("whiteboard coming soon!");
+  }
+
+  setupTabType() {
+    this.container.innerText = "";
+    switch (this.type) {
+      case "cards":
+        this.setupCards();
+        break;
+      case "images":
+        this.setupImages();
+        break;
+      case "stickyNotes":
+        this.setupStickyNotes();
+        break;
+      case "whiteboard":
+        this.setupWhiteboard();
+        break;
+    }
+  }
+
+  newTabType(icon) {
+    this.type = icon.id;
+    this.setupTabType();
   }
 }
