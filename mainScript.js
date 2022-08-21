439b654402c5d4eac3286ed3a35a39ee6881e10c
Tessa Grainger <tessa.robyn@outlook.com>
2022-08-21 18:50:14 +0100

    Added titles to localstorage

--- mainScript.js
+++ mainScript.js
@@ -7,6 +7,23 @@ function createPool(input) {
   window.location.href = "pool.html";
 }
 
+function loadData() {}
+
+function addExisting() {
+  let titles = localStorage.getItem("titles");
+  if (localStorage.getItem("titles") != null) {
+    titles = titles.split(",");
+    for (let i = 0; i < titles.length; i++) {
+      const p = document.createElement("p");
+      p.innerText = "Open " + titles[i];
+      document.body.append(p);
+      p.addEventListener("click", () => {
+        loadData();
+      });
+    }
+  }
+}
+
 function createNew(createButton) {
   const body = document.body;
   body.removeChild(createButton);
@@ -24,6 +41,7 @@ function createNew(createButton) {
   body.appendChild(container);
 }
 
+addExisting();
 const createButton = document.getElementById("createNew");
 createButton.addEventListener("click", () => {
   createNew(createButton);
--- poolScript.js
+++ poolScript.js
@@ -3,9 +3,30 @@ import { AddTab, Tab } from "./tab.js";
 function setName() {
   const title = document.getElementById("poolName");
   title.innerText = localStorage.getItem("currentPoolName");
+  return title.innerText;
 }
 
-setName();
+function saveData(title) {
+  let titles;
+  if (localStorage.getItem("titles") != null) {
+    titles = localStorage.getItem("titles").split(",");
+    if (!titles.includes(title)) {
+      titles = localStorage.getItem("titles") + "," + title;
+    } else {
+      titles = localStorage.getItem("titles");
+    }
+  } else {
+    titles = title;
+  }
+  localStorage.setItem("titles", titles);
+}
+
+const title = setName();
+
+const saveButton = document.getElementById("saveButton");
+saveButton.addEventListener("click", () => {
+  saveData(title);
+});
 
 const addTabButton = new AddTab();
 addTabButton.add("+");
